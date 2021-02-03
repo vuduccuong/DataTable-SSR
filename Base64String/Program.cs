a840473aff0cae97b9649b8be2c98dd86c59b22c
Cường VĐ <vuduccuong.ck48@gmail.com>
2021-02-03 20:58:06 +0700

    Encode and Decode Base64String

---
+++ Base64String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace Base64String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string strInput = Console.ReadLine();
+            
+            string strEncode = EncodeBase64(strInput);
+            string strDecode = DecodeBase64(strEncode);
+            Console.WriteLine("Chuỗi mã hoá: {0} ", strEncode);
+            Console.WriteLine("Chuỗi giải mã: {0} ", strDecode);
+            Console.ReadLine();
+        }
+
+        private static string DecodeBase64(string strEncode)
+        {
+            if (String.IsNullOrEmpty(strEncode))
+            {
+                return String.Empty;
+            }
+            var strBytes = Convert.FromBase64String(strEncode);
+            return Encoding.UTF8.GetString(strBytes);
+        }
+
+        private static string EncodeBase64(string strInput)
+        {
+            if (String.IsNullOrEmpty(strInput))
+            {
+                return String.Empty;
+            }
+            var strBytes = Encoding.UTF8.GetBytes(strInput);
+            return Convert.ToBase64String(strBytes);
+        }
+    }
+}
